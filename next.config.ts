81d7a280a38a4477db132098509bc885e79e393d
Supratim Sarkar <supratimsarkar@Supratims-MacBook-Air.local>
2025-10-03 10:12:49 +0400

    cool

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // Completely ignore TS errors during build
+    ignoreBuildErrors: true,
+  },
   images: {
     domains: [
-      'places.googleapis.com', // add this
-      // add any other external domains you need
+      "places.googleapis.com",
+      // Add any other external domains you need
     ],
   },
-
 };
 
 export default nextConfig;
